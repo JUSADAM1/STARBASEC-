47da05a100685d263a95797bbed858e614fae43d
JUSADAM1 <43390318+JUSADAM1@users.noreply.github.com>
2020-05-31 16:43:16 -0700

    Add files via upload

---
+++ starbase/starbase/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace starbase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_DisplayBase10 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Label_base_16 = new System.Windows.Forms.Label();
+            this.Label_display_base_2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(357, 350);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Gen New Code";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(212, 188);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Dec Base 10 Code";
+            // 
+            // label_DisplayBase10
+            // 
+            this.label_DisplayBase10.AutoSize = true;
+            this.label_DisplayBase10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_DisplayBase10.Location = new System.Drawing.Point(514, 188);
+            this.label_DisplayBase10.Name = "label_DisplayBase10";
+            this.label_DisplayBase10.Size = new System.Drawing.Size(86, 24);
+            this.label_DisplayBase10.TabIndex = 3;
+            this.label_DisplayBase10.Text = "No Code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(214, 232);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Hex Base 16 code";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(214, 283);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(175, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Binary Base 2 Code";
+            // 
+            // Label_base_16
+            // 
+            this.Label_base_16.AutoSize = true;
+            this.Label_base_16.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_base_16.Location = new System.Drawing.Point(514, 232);
+            this.Label_base_16.Name = "Label_base_16";
+            this.Label_base_16.Size = new System.Drawing.Size(86, 24);
+            this.Label_base_16.TabIndex = 6;
+            this.Label_base_16.Text = "No Code";
+            // 
+            // Label_display_base_2
+            // 
+            this.Label_display_base_2.AutoSize = true;
+            this.Label_display_base_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_display_base_2.Location = new System.Drawing.Point(514, 283);
+            this.Label_display_base_2.Name = "Label_display_base_2";
+            this.Label_display_base_2.Size = new System.Drawing.Size(86, 24);
+            this.Label_display_base_2.TabIndex = 7;
+            this.Label_display_base_2.Text = "No Code";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Cyan;
+            this.label4.Location = new System.Drawing.Point(199, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(451, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "SPACE STATION SECURITY SIMULATOR";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Cyan;
+            this.label5.Location = new System.Drawing.Point(54, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(722, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Hello User!! this will generate  a  bunch of random numbers for you to use as a p" +
+    "assword.";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(304, 131);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(271, 18);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Use The Binary Code as Password";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Label_display_base_2);
+            this.Controls.Add(this.Label_base_16);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label_DisplayBase10);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Starbase 13 security system";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_DisplayBase10;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Label_base_16;
+        private System.Windows.Forms.Label Label_display_base_2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ starbase/starbase/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace starbase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // When button is click(generate code button) 
+            // The curly braces will run
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // lower the random generator can go
+            int mini = 0;
+            // The highest the random number generator can go
+            int maxy = 1000;
+            // Gives a random number between the mini and the max
+            int randNumberBase10 = RandomNumber(mini, maxy);
+            // converts the number to base 2
+            string label_base2 = Convert.ToString(value: randNumberBase10, toBase: 2);
+            //converts to the base 16
+            string label_base16 = Convert.ToString(value: randNumberBase10, toBase: 16);
+            // Displays the number/character  in the labels
+            // technically where they are going to print
+            label_DisplayBase10.Text = randNumberBase10.ToString();
+            Label_base_16.Text = label_base16;
+            Label_display_base_2.Text = label_base2;
+
+        } 
+        public int RandomNumber(int mini, int maxy)
+        {
+            // Picks the  Random Number
+            Random random = new Random();
+            // Gives/returns you a random number
+            return random.Next(mini, maxy);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
